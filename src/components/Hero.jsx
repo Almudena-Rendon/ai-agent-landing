3a98c1a7a60fd401edaf470ff5aa14bbba77f15d
Almudena Rendón Fernández <126793941+Almudena-Rendon@users.noreply.github.com>
2025-07-19 19:05:36 +0200

    style: fix subtitle in hero section in mobile devices

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,7 @@ const Hero = () => {
       id="prologue"
       className="flex items-end justify-center w-screen h-screen bg-center bg-no-repeat bg-cover hero-img"
     >
-      <h1 className="pb-16 text-2xl font-bold uppercase md:pb-6 md:text-3xl text-blue-50">
+      <h1 className="pb-20 text-2xl font-bold uppercase md:pb-6 md:text-3xl text-blue-50">
         Love at <span className="text-[#010204]">first sight</span>
       </h1>
     </section>
