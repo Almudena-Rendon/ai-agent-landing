b145bd6989f9ae3bd03691f6eb76fbf60bdab4cb
Almudena Rendón Fernández <126793941+Almudena-Rendon@users.noreply.github.com>
2025-07-19 18:58:07 +0200

    style: fix title in hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,9 +2,9 @@ const Hero = () => {
   return (
     <section
       id="prologue"
-      className="flex items-end justify-center w-screen h-screen pb-4 bg-center bg-no-repeat bg-cover hero-img"
+      className="flex items-end justify-center w-screen h-screen bg-center bg-no-repeat bg-cover hero-img"
     >
-      <h1 className="text-2xl font-bold uppercase md:text-3xl text-blue-50">
+      <h1 className="pb-10 text-2xl font-bold uppercase md:pb-6 md:text-3xl text-blue-50">
         Love at <span className="text-[#010204]">first sight</span>
       </h1>
     </section>
