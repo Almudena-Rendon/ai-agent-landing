3139288c537fe0ba06ecbe6290c9785096050aa4
Almudena Rendón Fernández <126793941+Almudena-Rendon@users.noreply.github.com>
2025-07-20 19:51:52 +0200

    style: Change padding bottom in title in hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,7 @@ const Hero = () => {
       id="prologue"
       className="flex items-end justify-center w-screen h-screen bg-center bg-no-repeat bg-cover hero-img"
     >
-      <h1 className="pb-20 text-2xl font-bold uppercase md:pb-4 md:text-3xl text-blue-50 font-orbitron">
+      <h1 className="pb-20 text-2xl font-bold uppercase md:pb-6 md:text-3xl text-blue-50 font-orbitron">
         Tech at <span className="text-[#000204da]">first sight</span>
       </h1>
     </section>
